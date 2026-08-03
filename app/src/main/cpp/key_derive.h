eef25166e5338e1fa79d5a503b2f4035b15f2aa5
rendoyelaka <rendoyelaka@tutanota.de>
2026-08-03 17:32:12 +0530

    Update key_derive.h

--- app/src/main/cpp/key_derive.h
+++ app/src/main/cpp/key_derive.h
@@ -1,13 +1,10 @@
 #ifndef KEY_DERIVE_H
 #define KEY_DERIVE_H
-
 #include <stdint.h>
 #include <string.h>
-
-int derive_aes_key(
+int nova_derive_aes_key(
     const uint8_t android_id_hash[32],
     uint8_t out_key[32],
     uint8_t out_iv[12]
 );
-
 #endif
